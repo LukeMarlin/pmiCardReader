83c96c8f979b91add644a023e89942ebe0762b47
LukeMarlin <luke.marlin@viacesi.fr>
2013-11-18 16:07:08 +0100

    No more missing bit on the first tag passed

--- main.py
+++ main.py
@@ -18,24 +18,27 @@ TIMEOUT = FRAMETIME*(1+ALLOWANCE/100) #Real time allowed for the transmission
 
 #Method to convert the RFID binary value into a readable integer
 def binaryToInt(binary_string):
-	binary_string = binary_string[1:-1]
+	binary_string = binary_string[1:-1] #Removing the first and last bit (Non-data bits)
 	result = int(binary_string, 2)
 	return result
 
 #Method trigger after timer tick that prints out the tag
 def processTag():
 	global tag
+	if tag == "":
+		return
 	print("Frame of length (" + str(len(tag)) + "): " + tag + " (" + str(binaryToInt(tag)) + ")" )
 	tag = ""
 	
 def addBitToTag(gpio_id, val):
 	global tag
 	global t
-	
+
 	#Beginning of a new frame, we start the timer
 	if tag == "":
 		t = threading.Timer(TIMEOUT, processTag)
 		t.start()
+	
 	#We check wether we received a 0 or a 1
 	if gpio_id == GPIO_0:
 		tag += "0"
@@ -43,11 +46,18 @@ def addBitToTag(gpio_id, val):
 		tag += "1"		
 
 #We set the pins to Input
-RPIO.setup(GPIO_0, RPIO.IN)
-RPIO.setup(GPIO_1, RPIO.IN)
+RPIO.setup(GPIO_0, RPIO.IN, pull_up_down=RPIO.PUD_UP)
+RPIO.setup(GPIO_1, RPIO.IN, pull_up_down=RPIO.PUD_UP)
 
 #We register our callback on the pins events
 RPIO.add_interrupt_callback(GPIO_0, addBitToTag, edge='falling', pull_up_down=RPIO.PUD_UP)
 RPIO.add_interrupt_callback(GPIO_1, addBitToTag, edge='falling', pull_up_down=RPIO.PUD_UP)
+
+#Initializing timer
+t = threading.Timer(0.1, processTag)
+t.start()
+
+#Ready message
+print("Ready to go !")
 
 RPIO.wait_for_interrupts()
