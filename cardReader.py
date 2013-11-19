e658101b27bfbd1710cbf7c12b14e21bbcf36a2c
LukeMarlin <luke.marlin@viacesi.fr>
2013-11-19 17:35:39 +0100

    Added error checking for tags too short (<10 char) and parity check

--- cardReader.py
+++ cardReader.py
@@ -4,8 +4,9 @@ import threading
 class CardReader(object):
 	"""Class representing a reader. One object should be instantiated for each physical reader"""
 
-	def __init__(self, GPIO_0, GPIO_1, TIMEOUT):
+	def __init__(self, name, GPIO_0, GPIO_1, TIMEOUT):
 		#Pins used to receive 0s and 1s
+		self.name = name
 		self.GPIO_0 = GPIO_0
 		self.GPIO_1 = GPIO_1
 	
@@ -14,7 +15,6 @@ class CardReader(object):
 		return super().__init__()
 
 	def addBitToTag(self, gpio_id, val):
-
 		#Beginning of a new frame, we start the timer
 		if self.tag == "":
 			self.t = threading.Timer(self.TIMEOUT, self.processTag)
@@ -31,10 +31,7 @@ class CardReader(object):
 		RPIO.setup(self.GPIO_1, RPIO.IN)
 		RPIO.add_interrupt_callback(self.GPIO_0, self.addBitToTag, edge = edge, pull_up_down = pull_up_down)
 		RPIO.add_interrupt_callback(self.GPIO_1, self.addBitToTag, edge = edge, pull_up_down = pull_up_down)
-		
-		RPIO.input(self.GPIO_0)
-		RPIO.input(self.GPIO_1)
-    
+		 
 		#Initializing timer
 		self.t = threading.Timer(0.1, self.processTag)
 		self.t.start()
@@ -47,13 +44,34 @@ class CardReader(object):
 	def processTag(self):
 		if self.tag == "":
 			return
-		print("Frame of length (" + str(len(self.tag)) + "): " + self.tag + " (" + str(CardReader.binaryToInt(self.tag)) + ")" )
+		elif len(self.tag) < 10:
+			print("[" + self.name + "] Frame of length (" + str(len(self.tag)) + "):" + self.tag + " DROPPED")
+		elif self.verifyParity(self.tag):
+			print("[" + self.name + "] Frame of length (" + str(len(self.tag)) + "): " + self.tag + " (" + str(CardReader.binaryToInt(self.tag)) + ") OK KOI" )
+		
 		self.tag = ""
 
+	def verifyParity(self, binary_string):
+		first_part = binary_string[0:13]
+		second_part = binary_string[13:]
+		parts = [first_part, second_part]
+		bitsTo1 = [0, 0]
+		index = 0		
+
+		for part in parts:
+			bitsTo1[index] = part.count('1')
+			index += 1
+
+		if bitsTo1[0] % 2 != 0 or bitsTo1[1] % 2 != 1:
+			print("[" + self.name + "] Frame of length (" + str(len(self.tag)) + "): " + self.tag + " (" + str(CardReader.binaryToInt(self.tag)) + ") - PARITY CHECK FAILED")
+			return False
+		return True
 
 	#Method to convert the RFID binary value into a readable integer
 	@staticmethod
 	def binaryToInt(binary_string):
+		print(binary_string)
 		binary_string = binary_string[1:-1] #Removing the first and last bit (Non-data bits)
+		print(binary_string)
 		result = int(binary_string, 2)
 		return result
--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from cardReader import CardReader
 BIT_TRANSMISSION_TIME = 0.002 #From wiegand specification
 FRAMESIZE = 26 #Supposed size of received frame
 FRAMETIME = FRAMESIZE * BIT_TRANSMISSION_TIME #Theoric time necessary to transfer a frame
-ALLOWANCE = 50 #Auhtorized allowance for the transmission time in percent
+ALLOWANCE = 10 #Auhtorized allowance for the transmission time in percent
 TIMEOUT = FRAMETIME*(1+ALLOWANCE/100) #Real time allowed for the transmission
 
 #Creating readers
 readersList = [
-CardReader(23, 24, TIMEOUT),
-#CardReader(8, 7, TIMEOUT)
+CardReader("reader", 23, 24, TIMEOUT),
+CardReader("arduino", 8, 7, TIMEOUT)
 ]
 
 def closeProgram(signal, frame):
